3a14336093ae86b6b40de90f55990a4f4dca16a8
selvitopcu <selvi.kdb@gmail.com>
2021-01-01 16:10:09 +0300

    odev

---
+++ WebApplication6/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Kategoriler(Models.Kategorilerim bilgi)
+        {
+            var bil = new Models.Kategorilerim();
+            bil.KatAdi = "renkler";
+            bil.KatAciklama = "sogukrenkler";
+            bil.KatDetaylari = "soguk ve sicak renkler vardir";
+            bil.Numarasi = 101;
+
+            return View(bil);
+
+           
+        }
+    }
+}
---
+++ WebApplication6/Models/Kategorilerim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Models
+{
+    public class Kategorilerim
+    {
+        public int ID { get; set; }
+        public string KatAdi { get; set; }
+        public string KatDetaylari { get; set; }
+        public string KatAciklama { get; set; }
+        public int Numarasi { get; set; }
+
+    }
+}
